968eeff5c90f9ea63fd8462295c0011f91deaea1
Matt Tonks <matt.tonks@HeathPark.net>
2018-12-07 12:19:48 +0000

    Database seeding
    Fixes database seeding for pivot table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -20,5 +20,6 @@ class DatabaseSeeder extends Seeder
         $this->call(GroupUserTableSeeder::class);
         $this->call(ConcernTableSeeder::class);
         $this->call(CommentTableSeeder::class);
+        $this->call(ConcernStudentTableSeeder::class);
     }
 }

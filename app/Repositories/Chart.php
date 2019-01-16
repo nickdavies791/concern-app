73e24fd83c8fd54a8d22f8eb98b4749af61cb025
Nick Davies <nickdavies07@outlook.com>
2019-01-16 21:45:48 +0000

    Raw query builder to fetch months

--- app/Repositories/Chart.php
+++ app/Repositories/Chart.php
@@ -5,6 +5,7 @@ namespace App\Repositories;
 use App\Charts\DefaultChart;
 use App\Concern;
 use App\Tag;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 
 class Chart
@@ -28,6 +29,15 @@ class Chart
 
     public function testChart()
     {
+        $start = Carbon::createMidnightDate(Carbon::now()->subMonths(8)->year, 9, 1);
+        $end = Carbon::now();
+        $users = DB::table('concerns')
+            ->select(DB::raw('MONTHNAME(created_at) as month'))
+            ->whereBetween('created_at', [$start, $end])
+            ->groupBy('month')
+            ->get();
+        dd($users);
+
         $chart = $this->chart;
         $chart->labels(['A', 'B', 'C']);
         $chart->dataset('Test Dataset', 'bar', [200, 600, 75]);

6e58203baf6887ff03aa6bcee401cfa695442343
Nick Davies <nickdavies07@outlook.com>
2019-01-17 15:47:34 +0000

    Updated to use own class for each chart

--- app/Repositories/Chart.php
+++ app/Repositories/Chart.php
@@ -2,7 +2,7 @@
 
 namespace App\Repositories;
 
-use App\Charts\DefaultChart;
+use App\Charts\ConcernsByMonthBreakdown;
 use App\Concern;
 use App\Tag;
 use Carbon\Carbon;
@@ -10,34 +10,32 @@ use Illuminate\Support\Facades\DB;
 
 class Chart
 {
-    protected $chart;
     protected $tag;
     protected $concern;
 
     /**
      * Chart constructor.
-     * @param DefaultChart $chart
      * @param Tag $tag
      * @param Concern $concern
      */
-    public function __construct(DefaultChart $chart, Tag $tag, Concern $concern)
+    public function __construct(Tag $tag, Concern $concern)
     {
-        $this->chart = $chart;
         $this->tag = $tag;
         $this->concern = $concern;
     }
 
     /**
      * Return the chart data for concerns by month breakdown
-     * @return DefaultChart
+     * @return ConcernsByMonthBreakdown
      */
     public function concernsByMonthBreakdown()
     {
         // Set the start and end date
         $start = Carbon::createMidnightDate(Carbon::now()->subMonths(8)->year, 9, 1);
         $end = Carbon::now();
+        $year = $start->format('Y').'-'.$end->format('Y');
         // Query the database and return the total concerns by month between $start and $end
-        $users = DB::table('concerns')
+        $concerns = DB::table('concerns')
             ->select(DB::raw('MONTHNAME(created_at) as month, YEAR(created_at) as year, count(*) as total'))
             ->whereBetween('created_at', [$start, $end])
             ->where('deleted_at', null)
@@ -47,9 +45,9 @@ class Chart
                 return [$item->month => $item->total];
             });
         // Instantiate the chart and pass the key and values
-        $chart = $this->chart;
-        $chart->labels($users->keys()->all());
-        $chart->dataset('2018-2019', 'line', $users->values()->all())->options([
+        $chart = new ConcernsByMonthBreakdown;
+        $chart->labels($concerns->keys()->all());
+        $chart->dataset($year, 'line', $concerns->values()->all())->options([
             'backgroundColor' => '#5e72e4',
             'borderColor' => '#5e72e4',
             'borderWidth' => 4,

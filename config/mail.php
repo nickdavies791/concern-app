9a7c1c23867c43e84501b70741b63a26f0f0c349
Nick Davies <nickdavies07@outlook.com>
2019-02-11 08:37:44 +0000

    Updated mail.php

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'techsupport@clpt.co.uk'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 

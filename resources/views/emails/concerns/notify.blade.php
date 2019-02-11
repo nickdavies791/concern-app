c6d6c1a3d8b1458de9d4efd184adc5430d20e2c1
Nick Davies <nickdavies07@outlook.com>
2019-02-11 12:44:31 +0000

    Updated mailable markdown template

--- resources/views/emails/concerns/notify.blade.php
+++ resources/views/emails/concerns/notify.blade.php
@@ -1,17 +1,14 @@
 @component('mail::message')
-<p style="font-weight:lighter; font-size: 18px"> {{$user->name}} </p>
-<h1>Test</h1>
+<p style="font-weight:lighter; font-size: 18px">Hello, {{ $user->name }} </p>
+<h1>New Concern Logged</h1>
 <p style="font-weight: lighter; font-size: 18px line-height: 1.6">
 A new concern has been logged that requires your attention.
 </p>
 @component('mail::table')
 | Concern             | Occurred on                | Logged by   |
 | ------------------ |:------------------------- | ------------:|
-|{{$concern->title}} |{{$concern->concern_date}} |{{$loggedBy}} |
+|<a href="{{ route('concerns.show', ['id' => $concern->id]) }}">{{ $concern->title }}</a> |{{ $concern->concern_date }} |{{ $loggedBy }} |
 @endcomponent
 Regards,
 <p>Concern App</p>
-<hr>
-<a href="https://www.clpt.co.uk/">CLPT</a> |
-<a href="%unsubscribe_url%">Unsubscribe</a>
 @endcomponent

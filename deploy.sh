c0cf269144ffa3b2432f977c44541cfa6d3fe988
Nick Davies <nickdavies07@outlook.com>
2019-02-11 15:48:20 +0000

    Updated deployment script

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 # Trigger deployment
 # Replace the url below with your Forge/Laravel url
-curl -s ' https://forge.laravel.com/servers/264031/sites/705525/deploy/http?token=UL6uW49Rj3gYWEZ5kPK9TmyVnf2FhVZfLrQtYDqs ';
+curl -s 'https://forge.laravel.com/servers/264031/sites/705525/deploy/http?token=UL6uW49Rj3gYWEZ5kPK9TmyVnf2FhVZfLrQtYDqs';
 echo 'Deployed to Forge'

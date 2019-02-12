9b0c40db0fa6797d95a80dc4ab6cc37b2efa75a6
Nick Davies <nickdavies07@outlook.com>
2019-02-12 13:02:59 +0000

    Updated development.sh bash script to use wget

--- scripts/deploy/development.sh
+++ scripts/deploy/development.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 ## Deploy dev branch to development server
-curl "'https://forge.laravel.com/servers/264031/sites/705525/deploy/http?token='$FORGE_DEVELOPMENT_TOKEN";
-echo 'Deployed to Forge - Development';
+wget "https://forge.laravel.com/servers/264031/sites/705525/deploy/http?token="$FORGE_DEVELOPMENT_TOKEN;

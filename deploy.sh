f28ae625186ff29492abc236f3ec6a7f16db4ddc
Nick Davies <nickdavies07@outlook.com>
2019-02-12 11:50:48 +0000

    Updated curl request

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,11 @@
 ## Deploy dev branch to development server
 if [[ "$TRAVIS_BRANCH" -eq "dev" ]]; then
     ## Trigger deployment
-    curl 'https://forge.laravel.com/servers/264031/sites/705525/deploy/http?token='"$FORGE_DEVELOPMENT_TOKEN";
+    curl "'https://forge.laravel.com/servers/264031/sites/705525/deploy/http?token='$FORGE_DEVELOPMENT_TOKEN";
     echo 'Deployed to Forge - Development';
 ## Deploy master branch to production server
 elif [[ "$TRAVIS_BRANCH" -eq "master" ]]; then
     ## Trigger deployment
-    curl 'https://forge.laravel.com/servers/263849/sites/704463/deploy/http?token='"$FORGE_PRODUCTION_TOKEN";
+    curl "'https://forge.laravel.com/servers/263849/sites/704463/deploy/http?token='$FORGE_PRODUCTION_TOKEN";
     echo 'Deployed to Forge - Production';
 fi

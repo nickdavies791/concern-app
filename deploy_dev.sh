0990b050643ad38b52e6432a3ad68bcd6c93d10a
Nick Davies <nickdavies07@outlook.com>
2019-02-12 08:01:52 +0000

    Updated travis to deploy with conditionals

---
+++ deploy_dev.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Trigger deployment
+curl -s 'https://forge.laravel.com/servers/264031/sites/705525/deploy/http?token=IIhAgwcswjdLCsCd52pnxY0PMrZiwaN8rbEgKbnw';
+echo 'Deployed to Forge - concerns.dev.heathpark.net'
---
+++ deploy_prod.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Trigger deployment
+curl -s 'https://forge.laravel.com/servers/263849/sites/704463/deploy/http?token=Y9j2n2TW22drcoaKPJMPuU6hqq7ObQkNxRfb0IRi';
+echo 'Deployed to Forge - concerns.heathpark.net'

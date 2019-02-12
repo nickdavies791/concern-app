f3ba6f2b1e8f13f9d4f7a1d77d595ddebb4f0d02
Nick Davies <nickdavies07@outlook.com>
2019-02-12 13:21:39 +0000

    Added deploy command to travis.yml

--- scripts/deploy/production.sh
+++ scripts/deploy/production.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 ## Deploy master branch to production server
-curl "'https://forge.laravel.com/servers/263849/sites/704463/deploy/http?token='$FORGE_PRODUCTION_TOKEN";
-echo 'Deployed to Forge - Production';
+wget "https://forge.laravel.com/servers/263849/sites/704463/deploy/http?token="$FORGE_PRODUCTION_TOKEN;

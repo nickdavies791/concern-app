6e46ebe4ff32042ba5161515b4231d389acdacad
Nick Davies <nickdavies07@outlook.com>
2019-02-13 11:33:34 +0000

    Updated Dashboard for Staff Removed the charts from view for the staff and added an actions section with two action buttons; report a concern and view policies. Added a School Details section where the details for the school will be pulled from SIMS and displayed.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.argon')
 
 @section('content')
+    @adminOrSafeguarding
     <div class="row">
         <div class="col-xl-6">
             @component('partials.cards.card-chart')
@@ -19,4 +20,36 @@
             @endcomponent
         </div>
     </div>
+    @endadminOrSafeguarding
+    @staff
+    <div class="row">
+        <div class="col-xl-8">
+            @component('partials.cards.card')
+                @slot('title') School Details @endslot
+                @slot('body')
+                    <div class="card-body">
+                        <p>School info here...</p>
+                    </div>
+                @endslot
+            @endcomponent
+        </div>
+        <div class="col-xl-4">
+            @component('partials.cards.card')
+                @slot('title') Actions @endslot
+                @slot('body')
+                    <div class="card-body">
+                        <a href="{{ route('concerns.create') }}" class="btn btn-icon btn-3 btn-danger" type="button">
+                            <span class="btn-inner--icon"><i class="ni ni-chat-round"></i></span>
+                            <span class="btn-inner--text">Report a Concern</span>
+                        </a>
+                        <a href="{{ route('documents.index') }}" class="btn btn-icon btn-3 btn-primary" type="button">
+                            <span class="btn-inner--icon"><i class="ni ni-books"></i></span>
+                            <span class="btn-inner--text">View Policies</span>
+                        </a>
+                    </div>
+                @endslot
+            @endcomponent
+        </div>
+    </div>
+    @endstaff
 @endsection

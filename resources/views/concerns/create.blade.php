900b49ff8296eda0d064705b08f1efecd9f01802
Nick Davies <nickdavies07@outlook.com>
2019-02-14 12:34:09 +0000

    Restructured layout of inputs on concerns.create

--- resources/views/concerns/create.blade.php
+++ resources/views/concerns/create.blade.php
@@ -14,9 +14,23 @@
                                 <label>Find a Student</label>
                                 <student-select></student-select>
                             </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label>Observation or Disclosure</label>
+                                        <concern-type-select></concern-type-select>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label>Assign a tag</label>
+                                        <tag-select></tag-select>
+                                    </div>
+                                </div>
+                            </div>
                             <div class="form-group">
-                                <label>Observation or Disclosure</label>
-                                <concern-type-select></concern-type-select>
+                                <label>Date of Concern</label>
+                                <datetime required type="datetime" v-model="datetime" name="concern_date" input-id="concern_date" input-class="form-control" placeholder="Provide a date"></datetime>
                             </div>
                             <div class="form-group">
                                 <label>Details</label>
@@ -30,26 +44,12 @@
                                     <label class="custom-file-label border-0">Select</label>
                                 </div>
                             </div>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label>Date of Concern</label>
-                                        <datetime required type="datetime" v-model="datetime" name="concern_date" input-id="concern_date" input-class="form-control" placeholder="Provide a date"></datetime>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label>Assign a tag</label>
-                                        <tag-select></tag-select>
-                                    </div>
-                                </div>
-                            </div>
                             <div class="form-group">
                                 <a href="#" class="text-primary" data-toggle="modal" data-target="#body-map">Include a Body Map</a>
                                 <input type="hidden" id="url" name="image" value="">
                             </div>
                             <div class="form-group">
-                                <label>Notify Group(s)</label>
+                                <label>Assigned Groups</label>
                                 <group-select></group-select>
                             </div>
                             <button type="submit" class="btn btn-primary btn-lg" data-toggle="popover" data-trigger="hover" data-placement="right" data-content="Please check all details before saving. The Safeguarding Team will be notified.">Save Concern</button>

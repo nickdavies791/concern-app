64401258b047b8b0920863831500f76733ea1d10
Nick Davies <nickdavies07@outlook.com>
2019-02-14 15:03:54 +0000

    Removed commented code from web file

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,6 @@
 |
 */
 
-//$response = (new \App\Repositories\Assembly())->getAttendances();
-//
-//dd(json_decode($response));
-
 Auth::routes(['register' => false, 'reset' => false]);
 Route::get('/', 'HomeController@index');
 Route::get('/home', 'HomeController@index')->name('home');

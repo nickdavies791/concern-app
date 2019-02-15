a596bf090d87a1fdcb7e193ff4f357b0cac12bfe
Nick Davies <nickdavies07@outlook.com>
2019-02-15 09:30:07 +0000

    Restored missing Kernel file, added attendance scheduled job Restored the missing app\Console\Kernel.php file and added a new scheduled job command to get attendance from sims every day at 6am.

---
+++ app/Console/Kernel.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console;
+
+use App\Jobs\GetAttendanceFromSims;
+use App\Jobs\GetStaffMembersFromSims;
+use App\Jobs\GetStudentsFromSims;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        //
+    ];
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+        // Schedule Students Sync
+        $schedule->job(new GetStudentsFromSims())->weeklyOn(1, '6:00');
+        // Schedule Attendance Sync
+        $schedule->job(new GetAttendanceFromSims())->dailyAt('6:00');
+        // Schedule Staff Sync
+        $schedule->job(new GetStaffMembersFromSims())->weeklyOn(1, '6:00');
+    }
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        $this->load(__DIR__.'/Commands');
+        require base_path('routes/console.php');
+    }
+}

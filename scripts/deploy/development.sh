7f1a47b5dd2e48df75d2d881a6b1892118e40b49
Nick Davies <nickdavies07@outlook.com>
2019-03-12 10:26:26 +0000

    Update deployment script and yaml file
    Updates the deployment script to point to new Forge server. Removes the
    testing deployment temporarily from travis yaml file as no server set
    up.

--- scripts/deploy/development.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-## Deploy dev branch to development server
-wget "https://forge.laravel.com/servers/264031/sites/705525/deploy/http?token="$FORGE_DEVELOPMENT_TOKEN;
--- scripts/deploy/production.sh
+++ scripts/deploy/production.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 ## Deploy master branch to production server
-wget "https://forge.laravel.com/servers/263849/sites/704463/deploy/http?token="$FORGE_PRODUCTION_TOKEN;
+wget "https://forge.laravel.com/servers/270229/sites/723829/deploy/http?token="$FORGE_PRODUCTION_TOKEN;

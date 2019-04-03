f6c723ce6d52cdebc85cc6344356f6a7b97202e8
Nick Davies <nickdavies07@outlook.com>
2019-04-03 20:59:54 +0100

    Add random date for concern_date factory
    Adds a random date to the concern_date factory.

--- database/factories/ConcernFactory.php
+++ database/factories/ConcernFactory.php
@@ -8,7 +8,7 @@ $factory->define(App\Concern::class, function (Faker $faker) {
         'user_id' => factory('App\User')->create()->id,
         'type' => $faker->randomElement(['Observation', 'Disclosure']),
         'body' => $faker->paragraph(),
-        'concern_date' => now(),
+        'concern_date' => $faker->dateTimeBetween(Carbon::now()->subMonths(7)->toDateTimeString(), 'now')->format('Y-m-d h:i:s'),
         'resolved_on' => function() use($faker){
             return $faker->boolean(25) ? Carbon::now() : null;   
         }

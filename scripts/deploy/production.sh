3e9a2318fb2d5418a82343c58c84277ce79f1017
Nick Davies <nickdavies07@outlook.com>
2019-05-11 19:19:48 +0100

    Update production.sh

--- scripts/deploy/production.sh
+++ scripts/deploy/production.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 ## Deploy master branch to production server
-wget "https://forge.laravel.com/servers/270229/sites/726359/deploy/http?token="$FORGE_PRODUCTION_TOKEN;
+wget "https://forge.laravel.com/servers/"$FORGE_SERVER"/sites/"$FORGE_SITE"/deploy/http?token="$FORGE_PRODUCTION_TOKEN;
